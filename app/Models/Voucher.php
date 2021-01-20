b74b361b6f8a33b817d5a2ad7407576dd2931fd1
turky Eltahawy <turkytahawy3030@gmail.com>
2021-01-20 19:52:51 +0200

    review

---
+++ app/Models/Voucher.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Dimsav\Translatable\Translatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Voucher extends Model
+{
+    use Translatable;
+    use SoftDeletes;
+    protected $table = 'vouchers';
+    public $translatedAttributes = ['title'];
+    protected $fillable = ['code','parent_id', 'expire_date', 'number', 'value', 'vendor_id', 'branch_id', 'activity_id', 'segmentation_id', 'company_id', 'is_used', 'active'];
+
+    public function used($voucher)
+    {
+        return Voucher::where([['parent_id', $voucher], ['is_used', 1]])->count();
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+}
